0e92fa8ee7922f90749a738b3e35faef4d93017d
maratovaruuke <myrzalievaruuke1@gmail.com>
2023-11-21 18:35:21 +0600

    created the project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace homework_70_aruuke_maratova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
